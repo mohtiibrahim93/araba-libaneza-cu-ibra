0388d5caa378d2b7b286009b3ce6e74f9df44d1a
mohtiibrahim93 <mohtiibrahim@gmail.com>
2026-09-17 15:38:09 +0300

    feat(ios): validate root and morphology references

--- ios-app/Sources/YallaCore/Content/ContentValidator.swift
+++ ios-app/Sources/YallaCore/Content/ContentValidator.swift
@@ -3,6 +3,8 @@ public enum ContentValidationError: Error, Equatable, Sendable {
     case duplicateUnitID(String)
     case duplicateExerciseID(String)
     case duplicateLexiconCollectionID(String)
+    case duplicateRootID(String)
+    case duplicateMorphologicalPatternID(String)
     case missingExpressionReference(unitID: String, expressionID: String)
     case missingDefaultLocalization(expressionID: String, locale: String)
     case missingDefaultUnitLocalization(unitID: String, locale: String)
@@ -10,6 +12,12 @@ public enum ContentValidationError: Error, Equatable, Sendable {
     case missingExerciseExpressionReference(exerciseID: String, expressionID: String)
     case missingDefaultLexiconCollectionLocalization(collectionID: String, locale: String)
     case missingLexiconExpressionReference(collectionID: String, expressionID: String)
+    case missingMorphologyExpressionReference(expressionID: String)
+    case missingMorphologyRootReference(rootID: String)
+    case missingMorphologyPatternReference(patternID: String)
+    case missingInflectionSourceExpressionReference(expressionID: String)
+    case missingInflectionTargetExpressionReference(expressionID: String)
+    case missingInflectionPatternReference(patternID: String)
 }
 
 public struct ContentValidator: Sendable {
@@ -77,6 +85,44 @@ public struct ContentValidator: Sendable {
                     exerciseID: exercise.id,
                     expressionID: expressionID
                 )
+            }
+        }
+
+        var rootIDs = Set<String>()
+        for root in package.roots {
+            guard rootIDs.insert(root.id).inserted else {
+                throw ContentValidationError.duplicateRootID(root.id)
+            }
+        }
+
+        var patternIDs = Set<String>()
+        for pattern in package.morphologicalPatterns {
+            guard patternIDs.insert(pattern.id).inserted else {
+                throw ContentValidationError.duplicateMorphologicalPatternID(pattern.id)
+            }
+        }
+
+        for link in package.morphologyLinks {
+            guard expressionIDs.contains(link.expressionID) else {
+                throw ContentValidationError.missingMorphologyExpressionReference(expressionID: link.expressionID)
+            }
+            guard rootIDs.contains(link.rootID) else {
+                throw ContentValidationError.missingMorphologyRootReference(rootID: link.rootID)
+            }
+            if let patternID = link.patternID, !patternIDs.contains(patternID) {
+                throw ContentValidationError.missingMorphologyPatternReference(patternID: patternID)
+            }
+        }
+
+        for relation in package.inflectionRelations {
+            guard expressionIDs.contains(relation.sourceExpressionID) else {
+                throw ContentValidationError.missingInflectionSourceExpressionReference(expressionID: relation.sourceExpressionID)
+            }
+            guard expressionIDs.contains(relation.targetExpressionID) else {
+                throw ContentValidationError.missingInflectionTargetExpressionReference(expressionID: relation.targetExpressionID)
+            }
+            if let patternID = relation.patternID, !patternIDs.contains(patternID) {
+                throw ContentValidationError.missingInflectionPatternReference(patternID: patternID)
             }
         }
     }
