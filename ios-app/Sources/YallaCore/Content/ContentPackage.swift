e8c2e128b511c1d38d1e6c2edfe27667c594f299
mohtiibrahim93 <mohtiibrahim@gmail.com>
2026-09-17 13:11:38 +0300

    feat(ios): include exercises in content packages

--- ios-app/Sources/YallaCore/Content/ContentPackage.swift
+++ ios-app/Sources/YallaCore/Content/ContentPackage.swift
@@ -2,10 +2,17 @@ public struct ContentPackage: Codable, Equatable, Sendable {
     public let manifest: ContentManifest
     public let expressions: [Expression]
     public let units: [JourneyUnit]
+    public let exercises: [ExerciseDefinition]
 
-    public init(manifest: ContentManifest, expressions: [Expression], units: [JourneyUnit]) {
+    public init(
+        manifest: ContentManifest,
+        expressions: [Expression],
+        units: [JourneyUnit],
+        exercises: [ExerciseDefinition] = []
+    ) {
         self.manifest = manifest
         self.expressions = expressions
         self.units = units
+        self.exercises = exercises
     }
 }
