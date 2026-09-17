46ad65c4693af371194ff7dcf89d6c48f4c5af4f
mohtiibrahim93 <mohtiibrahim@gmail.com>
2026-09-17 13:11:30 +0300

    feat(ios): localize Journey units

--- ios-app/Sources/YallaCore/Content/JourneyUnit.swift
+++ ios-app/Sources/YallaCore/Content/JourneyUnit.swift
@@ -4,14 +4,38 @@ public enum LevelBand: String, Codable, Equatable, Sendable {
     case b1Partial = "b1-partial"
 }
 
+public struct JourneyUnitLocalization: Codable, Equatable, Sendable {
+    public let title: String
+    public let description: String
+
+    public init(title: String, description: String) {
+        self.title = title
+        self.description = description
+    }
+}
+
 public struct JourneyUnit: Codable, Equatable, Sendable, Identifiable {
     public let id: String
     public let level: LevelBand
     public let expressionIDs: [String]
+    public let localizations: [String: JourneyUnitLocalization]
 
-    public init(id: String, level: LevelBand, expressionIDs: [String]) {
+    public init(
+        id: String,
+        level: LevelBand,
+        expressionIDs: [String],
+        localizations: [String: JourneyUnitLocalization]
+    ) {
         self.id = id
         self.level = level
         self.expressionIDs = expressionIDs
+        self.localizations = localizations
+    }
+
+    public func localization(for locale: String) throws -> JourneyUnitLocalization {
+        guard let localization = localizations[locale] else {
+            throw LocalizationError.missingLocale(locale)
+        }
+        return localization
     }
 }
