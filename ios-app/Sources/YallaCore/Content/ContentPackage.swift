05db4738e7d6866285c7f85b6c434a2eda7d7c94
mohtiibrahim93 <mohtiibrahim@gmail.com>
2026-09-17 15:37:31 +0300

    feat(ios): include morphology in content packages

--- ios-app/Sources/YallaCore/Content/ContentPackage.swift
+++ ios-app/Sources/YallaCore/Content/ContentPackage.swift
@@ -4,23 +4,36 @@ public struct ContentPackage: Codable, Equatable, Sendable {
     public let units: [JourneyUnit]
     public let exercises: [ExerciseDefinition]
     public let lexiconCollections: [LexiconCollection]
+    public let roots: [Root]
+    public let morphologicalPatterns: [MorphologicalPattern]
+    public let morphologyLinks: [MorphologyLink]
+    public let inflectionRelations: [InflectionRelation]
 
     public init(
         manifest: ContentManifest,
         expressions: [Expression],
         units: [JourneyUnit],
         exercises: [ExerciseDefinition] = [],
-        lexiconCollections: [LexiconCollection] = []
+        lexiconCollections: [LexiconCollection] = [],
+        roots: [Root] = [],
+        morphologicalPatterns: [MorphologicalPattern] = [],
+        morphologyLinks: [MorphologyLink] = [],
+        inflectionRelations: [InflectionRelation] = []
     ) {
         self.manifest = manifest
         self.expressions = expressions
         self.units = units
         self.exercises = exercises
         self.lexiconCollections = lexiconCollections
+        self.roots = roots
+        self.morphologicalPatterns = morphologicalPatterns
+        self.morphologyLinks = morphologyLinks
+        self.inflectionRelations = inflectionRelations
     }
 
     private enum CodingKeys: String, CodingKey {
         case manifest, expressions, units, exercises, lexiconCollections
+        case roots, morphologicalPatterns, morphologyLinks, inflectionRelations
     }
 
     public init(from decoder: Decoder) throws {
@@ -30,5 +43,9 @@ public struct ContentPackage: Codable, Equatable, Sendable {
         units = try container.decode([JourneyUnit].self, forKey: .units)
         exercises = try container.decodeIfPresent([ExerciseDefinition].self, forKey: .exercises) ?? []
         lexiconCollections = try container.decodeIfPresent([LexiconCollection].self, forKey: .lexiconCollections) ?? []
+        roots = try container.decodeIfPresent([Root].self, forKey: .roots) ?? []
+        morphologicalPatterns = try container.decodeIfPresent([MorphologicalPattern].self, forKey: .morphologicalPatterns) ?? []
+        morphologyLinks = try container.decodeIfPresent([MorphologyLink].self, forKey: .morphologyLinks) ?? []
+        inflectionRelations = try container.decodeIfPresent([InflectionRelation].self, forKey: .inflectionRelations) ?? []
     }
 }
